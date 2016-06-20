cb9624be0f4ccc82ca3a020291a3ea1a6c207c32
olmesm <olmesm@gmail.com>
2016-06-20 13:17:59 +0100

    drawing player body

--- game.js
+++ game.js
@@ -4,6 +4,8 @@
     var screen = canvas.getContext('2d');
     var gameSize = { x: canvas.width, y: canvas.height };
 
+    this.bodies = [ new Player(this, gameSize) ];
+
     var self = this;
     var tick = function() {
       self.update();
@@ -23,8 +25,26 @@
     },
 
     draw: function(screen, gameSize) {
-
+      for (var i = 0; i < this.bodies.length; i++) {
+        drawRect(screen, this.bodies[i]);
+      };
     }
+  };
+
+  var Player = function(game, gameSize) {
+    this.game = game;
+    this.size = { x: 15, y: 15 };
+    this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x };
+  };
+
+  Player.prototype = {
+    update: function() {}
+  };
+
+  var drawRect = function(screen, body) {
+    screen.fillRect(body.center.x - body.size.x / 2,
+                    body.center.y - body.size.y / 2,
+                    body.size.x, body.size.y);
   };
 
   window.onload = function() {
