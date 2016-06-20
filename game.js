d13111b924d4d38058998b15177c4270713ec2bb
olmesm <olmesm@gmail.com>
2016-06-20 13:31:58 +0100

    keyboard mapped and moving player

--- game.js
+++ game.js
@@ -21,7 +21,9 @@
     // assigning a function as a property of an object literal
     // http://stackoverflow.com/questions/9384865/javascript-colon-for-labeling-annonymous-functions
     update: function() {
-
+      for (var i = 0; i < this.bodies.length; i++) {
+        this.bodies[i].update();
+      };
     },
 
     draw: function(screen, gameSize) {
@@ -35,10 +37,17 @@
     this.game = game;
     this.size = { x: 15, y: 15 };
     this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x };
+    this.keyboarder = new Keyboarder();
   };
 
   Player.prototype = {
-    update: function() {}
+    update: function() {
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+        this.center.x -= 2;
+      } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+        this.center.x += 2;
+      }
+    }
   };
 
   var drawRect = function(screen, body) {
@@ -56,6 +65,16 @@
 
     window.onkeyup = function(event) {
       keyState[event.keyCode] = false;
+    };
+
+    this.isDown = function(keyCode) {
+      return keyState[keyCode] === true;
+    };
+
+    this.KEYS = {
+      LEFT: 37,
+      RIGHT: 39,
+      SPACE: 32
     };
   };
 
